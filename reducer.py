9d6332707abd26938fc227b5a0047831b8c8fa27
Kamilla <s_nauruzbayeva23@stud.hwr-berlin.de>
2024-07-14 16:16:09 +0200

    5. Change the reducer, so that only categories are shown, that have a total number (count) of more than 114 purchases

--- reducer.py
+++ reducer.py
@@ -10,10 +10,10 @@ for line in sys.stdin:
     if current_category == category:
         current_count += 1
     else:
-        if current_category:
+        if current_category and current_count > 114:
             sys.stdout.write("{0}\t{1}\n".format(current_category, current_count))
         current_category = category
         current_count = 1
 
-if current_category:
+if current_category and current_count > 114:
     sys.stdout.write("{0}\t{1}\n".format(current_category, current_count))

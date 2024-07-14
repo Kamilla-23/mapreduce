36cd37efd49a54ded19a95465f2184528665103c
Kamilla <s_nauruzbayeva23@stud.hwr-berlin.de>
2024-07-14 16:23:26 +0200

    6. Change the reducer so that you calculate the average sales per category

--- reducer.py
+++ reducer.py
@@ -3,17 +3,23 @@
 import sys
 
 current_category = None
+current_sum = 0
 current_count = 0
 
 for line in sys.stdin:
-    category, _ = line.strip().split("\t")
+    category, amount = line.strip().split("\t")
+    amount = float(amount)
     if current_category == category:
+        current_sum += amount
         current_count += 1
     else:
-        if current_category and current_count > 114:
-            sys.stdout.write("{0}\t{1}\n".format(current_category, current_count))
+        if current_category:
+            average = current_sum / current_count
+            sys.stdout.write("{0}\t{1}\n".format(current_category, average))
         current_category = category
+        current_sum = amount
         current_count = 1
 
-if current_category and current_count > 114:
-    sys.stdout.write("{0}\t{1}\n".format(current_category, current_count))
+if current_category:
+    average = current_sum / current_count
+    sys.stdout.write("{0}\t{1}\n".format(current_category, average))
